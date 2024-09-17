e3f1a4cc613c88fdb97fa1bd1c26d976650a70d5
kyu-3hiya4 <177545609+kyu-3hiya4>
2024-09-17 14:03:39 +0000

    [Add] 4-24 追加

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -6,7 +6,7 @@ module.exports = environment
 const webpack = require('webpack')
 environment.plugins.prepend(
   'Provide',
-  new webpack.ProbidePlugin({
+  new webpack.ProvidePlugin({
     $: 'jquery/src/jquery',
     jQuery: 'jquery/src/jquery',
     Popper: 'popper.js'

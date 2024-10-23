a66a156eb57eb5f756903e988d5a083a1aa296a0
kyu-3hiya4 <177545609+kyu-3hiya4>
2024-10-23 09:04:29 +0000

    [add]管理者ログアウト機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  devise_for :admin, skip: [:registrations, :password], controllers: {
+    sessions: 'admin/sessions'
+  }
+
+  namespace :admin do
+    get 'dashboards', to: 'dashboards#index'
+    resources :users, only: [:destroy]
+  end
+
   root to: "homes#top"
   get 'homes/about', as:'about'
   resources :post_images, only: [:new, :create, :index, :show, :destroy] do
